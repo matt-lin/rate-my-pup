6d28885a433b922c6a8089a03b66935df120af6c
mcgourtyalex <mcgourty.alex@gmail.com>
2015-03-06 13:40:37 -0800

    controller pup creation

--- app/controllers/pups_controller.rb
+++ app/controllers/pups_controller.rb
@@ -1,5 +1,18 @@
 class PupsController < ApplicationController
+
   def index
     @pups = Pup.all
   end
+
+  def new
+
+  end
+
+  def create
+    @pup = Pup.create!(params[:pup])
+    flash(:notice) = "#{@pup.pup_name} was successfully added"
+    redirect_to pups_path
+  end
+
+
 end
