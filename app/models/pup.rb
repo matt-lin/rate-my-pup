a04707c4295e072a1c25f560d7456d02c13c5f0d
eabartlett <eabartlett@berkeley.edu>
2015-03-08 16:03:23 -0700

    Merging stash after initial rspec tests

--- app/models/pup.rb
+++ app/models/pup.rb
@@ -1,8 +1,8 @@
 class Pup < ActiveRecord::Base
 
   belongs_to :breeder
-
-  attr_accessible :pup_name, :owner_name, :breeder, :breed_1, :breed_2, :breeder_responsibility, :overall_health, :trainability, :social_behavior, :energy_level, :simpatico_rating, :comments, :kennel
+  
+	attr_accessible :pup_name, :owner_name, :breeder, :breed_1, :breed_2, :breeder_responsibility, :overall_health, :trainability, :social_behavior, :energy_level, :simpatico_rating, :comments, :kennel
 
   def Pup.find_by_breeds(breed1, breed2 = nil)
     if breed2
---
+++ spec/factories/pup.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :pup do
+    pup_name 'Doge'
+    owner_name 'Curious George'
+    breeder 'Teddy Roosevelt'
+    breed_1 'Shiba Inu'
+    breeder_responsibility 4
+    overall_health 5
+    trainability 4
+    social_behavior 5
+    energy_level 1
+    simpatico_rating 5
+    comments "Wow. Such doge. Very pup."
+  end
+end
