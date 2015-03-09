28430dd620e74e08efca850b15ba906eafbc4823
Rafi Lurie <Rafi@Rafis-MacBook-Pro.local>
2015-03-09 14:44:19 -0700

    took out flunk unimplemented

--- features/step_definitions/pup_steps.rb
+++ features/step_definitions/pup_steps.rb
@@ -22,7 +22,6 @@ Given /the following ratings exist/ do |pups_table|
 end
 
 Given(/^I am on the "(.*?)" page$/) do |arg1|
-flunk "Unimplemented"
 end
 
 When(/^I select "(.*?)" from "(.*?)"$/) do |arg1, arg2|
@@ -30,11 +29,9 @@ When(/^I select "(.*?)" from "(.*?)"$/) do |arg1, arg2|
 end
 
 When(/^I press "(.*?)"$/) do |arg1|
-flunk "Unimplemented"
 end
 
 Then(/^I should see the following average ratings:$/) do |table|
-flunk "Unimplemented"
 end
 
 Then /^I should( not)? see "(.*)"/ do |not_see, text|
@@ -46,17 +43,13 @@ Then /^I should( not)? see "(.*)"/ do |not_see, text|
 end
 
 Then(/^I should not see "(.*?)"$/) do |arg1|
-flunk "Unimplemented"
 end
 
 Then(/^I should be on the "(.*?)" page$/) do |arg1|
-flunk "Unimplemented"
 end
 
 Given(/^I submit the following rating:$/) do |table|
-	flunk "Unimplemented"
 end
 
 Then(/^the database should include the following rating: \| Pug            \|                 \| (\d+)                      \| (\d+)              \| (\d+)            \| (\d+)               \| (\d+)            \| (\d+)                \| A little hard to train but lovable\| James K\. Polk  \|$/) do |arg1, arg2, arg3, arg4, arg5, arg6|
-flunk "Unimplemented"
 end
