d93bacb914846a8a8861f7cbeecdd30db32bffb9
mcgourtyalex <mcgourty.alex@gmail.com>
2015-03-08 17:03:27 -0700

    added more controller methods for the pups, and a single pup view

--- app/controllers/pups_controller.rb
+++ app/controllers/pups_controller.rb
@@ -8,9 +8,25 @@ class PupsController < ApplicationController
 
   end
 
+  def show
+    @pup = Pup.find params[:id]
+  end
+
   def create
     @pup = Pup.create!(params[:pup])
     flash[:notice] = "#{@pup.pup_name} was successfully added"
+    redirect_to pups_path
+  end
+
+  def update
+    @pup = Pup.find params[:id]
+    @pup.update_attributes(params[:pup])
+    redirect_to pups_path
+  end
+
+  def destroy
+    @pup = Pup.find params[:id]
+    @pup.destroy
     redirect_to pups_path
   end
 
