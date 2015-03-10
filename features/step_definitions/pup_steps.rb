69959f802cb19751dbf99aec14ba7784d25e87eb
annagoodman <aegoodman@berkeley.edu>
2015-03-10 16:01:49 -0700

    submit rating step completed

--- features/step_definitions/pup_steps.rb
+++ features/step_definitions/pup_steps.rb
@@ -57,7 +57,22 @@ end
 Then /^I should be on the "(.*?)" page/ do |arg1|
 end
 
-Given /^I submit the following rating:$/ do |table|
+Given /^I submit the following rating:$/ do |pups_table|
+  pups_table.hashes.each do |rating|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+    Pup.create(
+      breed_1: rating['breed_1'],
+      breed_2: rating['breed_2'],
+      breeder_responsibility: rating['breeder_responsibility'],
+      overall_health: rating['overall_health'],
+      trainability: rating['trainability'],
+      social_behavior: rating['social_behavior'],
+      energy_level: rating['energy_level'],
+      simpatico_rating: rating['simpatico_rating'],
+      comments: rating['comments'],
+      breeder_id: rating['breeder_id'])
+  end
 end
 
 Then /^the database should include the following rating: \| Pug            \|                 \| (\d+)                      \| (\d+)              \| (\d+)            \| (\d+)               \| (\d+)            \| (\d+)                \| A little hard to train but lovable\| James K\. Polk  \|$/ do |arg1, arg2, arg3, arg4, arg5, arg6|
