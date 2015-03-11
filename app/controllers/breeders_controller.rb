3cb9364648fc70feec8f2ce68b39a8052dee90cf
eabartlett <eabartlett@berkeley.edu>
2015-03-11 14:12:54 -0700

    Breeder hooked up to main

--- app/controllers/breeders_controller.rb
+++ app/controllers/breeders_controller.rb
@@ -1,7 +1,8 @@
 class BreedersController < ApplicationController
   def search_name
-    @breeder = Breeder.find_by_name(params[:breeder_name])
+    @breeder = Breeder.find_by_name(params[:breeders][:breeder_name])
     if !@breeder
+      flash[:message] = "Sorry there are not reviews for #{params[:breeders][:breeder_name]}"
       redirect_to root_path and return
     end
     @pups = @breeder.pups
--- spec/controllers/breeders_controller_spec.rb
+++ spec/controllers/breeders_controller_spec.rb
@@ -7,11 +7,11 @@ describe BreedersController do
       dogs = 10.times {FactoryGirl.build(:pup, :breeder_id => breeder.id)}
       Breeder.should_receive(:find_by_name).with("Teddy Roosevelt").and_return(breeder)
       breeder.should_receive(:pups).and_return(dogs)
-      get :search_name, {:breeder_name => "Teddy Roosevelt"}
+      get :search_name, {:breeders => {:breeder_name => "Teddy Roosevelt"}}
     end
     it "should redirect to main page if breeder doesn't exist" do
       Breeder.should_receive(:find_by_name).with("Erik Bartlett").and_return(nil)
-      get :search_name, {:breeder_name => "Erik Bartlett"}
+      get :search_name, {:breeders =>{:breeder_name => "Erik Bartlett"}}
       response.should redirect_to root_path
     end
   end
