128fc9e62f75a2a973a7ae1a7c158aac756b5bb8
Rafi Lurie <Rafi@Rafis-MacBook-Pro.local>
2015-04-01 22:30:34 -0700

    fixed selenium cucumber problem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,7 @@ Ratemypup::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.serve_static_assets = false
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
