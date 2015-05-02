d07b517351ebd0e4b95a9ab0c4bd70251c55bf15
eabartlett <eabartlett@berkeley.edu>
2015-05-01 18:28:05 -0700

    Hashtags

--- db/migrate/20150429212818_add_hashtags_to_pup.rb
+++ db/migrate/20150429212818_add_hashtags_to_pup.rb
@@ -1,8 +1,5 @@
 class AddHashtagsToPup < ActiveRecord::Migration
   def change
-    remove_column :pups, :hashtag_1
-    remove_column :pups, :hashtag_2
-    remove_column :pups, :hashtag_3
     add_column :pups, :hashtag_1, :string
     add_column :pups, :hashtag_2, :string
     add_column :pups, :hashtag_3, :string
