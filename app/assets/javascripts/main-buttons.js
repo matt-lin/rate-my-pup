be01a9bbd7203f0d755c2878a30fd7dc10ed46d6
eabartlett <eabartlett@berkeley.edu>
2015-05-07 22:28:20 -0700

    Fix unclosed strong tag

--- app/assets/javascripts/main-buttons.js
+++ app/assets/javascripts/main-buttons.js
@@ -7,7 +7,7 @@ var MainButtons = {
     // setup sets a callback for #breeder_find keyup
     setup: function() {
         $('.button-a').hover( function() {
-            $('#tagline-text').html("Contribute information about <strong>your<strong> dog to our database");
+            $('#tagline-text').html("Contribute information about <strong>your</strong> dog to our database");
         });
 
         $('.button-b').click( function() {
