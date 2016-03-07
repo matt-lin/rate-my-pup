effd0b81e2fcaca1ff3d41509c0fda52db311caa
Qiyin Wu <qiyin.wu.jor@gmail.com>
2016-03-06 16:39:52 -0800

    readded the comment

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -19,6 +19,7 @@ module NavigationHelpers
     when /^the Login page$/ then '/users/sign_in'
     when /^the "Add breeder" page$/ then '/breeders/new'
     when /^the Breed page$/ then '/breed'
+    # Add more mappings here
     when /^the "Dog Name" page$/ then '/pups/dog_name'
     when /^the "Dog How Long" page$/ then '/pups/dog_how_long'
     when /^the "Dog Breed" page$/ then 'pups/dog_breed'
