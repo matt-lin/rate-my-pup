2ff560cbc6db6dce97a1d8c8ea98dccd525f4d07
Jianzhong Chen <harrychen-cjz@hotmail.com>
2016-04-15 03:26:40 +0000

    add user agreement support

--- db/migrate/20160414183506_add_username_to_users.rb
+++ db/migrate/20160414183506_add_username_to_users.rb
@@ -1,6 +1,5 @@
 class AddUsernameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :username, :string, null: false, default: ""
-    add_index :users, :username, unique: true
   end
 end
