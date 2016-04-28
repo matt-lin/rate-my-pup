3006559d34213f2455b72218e4e8d836ae6a7199
cadytxu <cadytxu@gmail.com>
2016-04-28 18:07:37 +0000

    made button size smaller, used the original background picture as customer requested

--- app/assets/javascripts/puppy_slider.js
+++ app/assets/javascripts/puppy_slider.js
@@ -2,27 +2,27 @@
  * Created by mcgourtyalex on 4/20/15.
  */
 
-// var PupSlider = {
+var PupSlider = {
 
-//     // setup sets a callback for #breeder_find keyup
-//     setup: function() {
-//         PupSlider.puppy_change(7);
-//         /*var pup_num = 2;
-//         var interval = setInterval(function() {
-//             console.log("hey!");
-//             PupSlider.puppy_change((pup_num++ + 1) % 7 + 1);
-//         }, 5000);
-//         */
-//     },
+    // setup sets a callback for #breeder_find keyup
+    setup: function() {
+        PupSlider.puppy_change(7);
+        /*var pup_num = 2;
+        var interval = setInterval(function() {
+            console.log("hey!");
+            PupSlider.puppy_change((pup_num++ + 1) % 7 + 1);
+        }, 5000);
+        */
+    },
 
-//     puppy_change: function(pup_num) {
-//         if (window.location.pathname == "/") {
-//             $('body').css("background-image", "url('/assets/pup" + pup_num + ".jpg')");
-//         }
-//     }
-// };
+    puppy_change: function(pup_num) {
+        if (window.location.pathname == "/") {
+            $('body').css("background-image", "url('/assets/pup" + pup_num + ".jpg')");
+        }
+    }
+};
 
 
-// $(document).ready(function () {
-//     $(PupSlider.setup);
-// });
+$(document).ready(function () {
+    $(PupSlider.setup);
+});
