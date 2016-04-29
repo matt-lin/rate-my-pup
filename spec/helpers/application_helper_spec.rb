1d2ee51f8992a889c170eb52391525dfe9aa210c
Jianzhong Chen <harrychen-cjz@hotmail.com>
2016-04-29 04:01:20 +0000

    add test for application helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -1,9 +1,9 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe ApplicationHelper, :type => :helper do
   describe "bootstrap class" do
     it "returns the correct class according to flash type" do
-      ApplicationHelper.bootstrap_class_for(:success).should eq("alert-success")
+      bootstrap_class_for(:success).should eq("alert-success")
       bootstrap_class_for(:error).should eq("alert-error")
       bootstrap_class_for(:alert).should eq("alert-block")
       bootstrap_class_for(:notice).should eq("alert-info")
