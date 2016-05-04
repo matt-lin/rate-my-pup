8a6f53948b3dd08240c72662b75271dab1dea322
cadytxu <cadytxu@gmail.com>
2016-05-04 00:27:18 +0000

    fixed tests

--- app/controllers/breeders_controller.rb
+++ app/controllers/breeders_controller.rb
@@ -17,7 +17,7 @@ class BreedersController < ApplicationController
   def search_name
     @breeder = Breeder.find_by_name(params[:breeders][:breeder_name])
     if !@breeder
-      flash[:message] = "Sorry there are not reviews for #{params[:breeders][:breeder_name]}"
+      flash[:message] = "Sorry, we do not yet have any ratings #{params[:breeders][:breeder_name]}"
       redirect_to root_path and return
     end
     @avg_ratings = @breeder.avg_pup_rating
