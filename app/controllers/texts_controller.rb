7b3f19bc934569fbbb2dfb1180af51924df8e669
cadytxu <cadytxu@gmail.com>
2016-06-06 00:16:00 +0000

    article title is displayed dynamically

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,12 +1,19 @@
 class TextsController < ApplicationController
     
     def mission
-        @mission_text = Text.find_by_title("Mission")
+        @mission_text = Text.find_by_id(1)
     end 
     
     def background
-        @background_text = Text.find_by_title("Background")
+        @background_text = Text.find_by_id(2)
     end 
     
+    def goals
+        @goals_text = Text.find_by_id(3)
+    end
+    
+    def how_you_can_help
+        @how_you_can_help_text = Text.find_by_id(4)
+    end
 end
   
